789b8256a40c4290a2ff037015c7c9f9d5b68b2a
Valdemar <124436@student.drenthecollege.nl>
2019-02-12 19:11:28 +0100

    opdracht 8

--- studieblok34/opdracht8/opdracht8.js
+++ studieblok34/opdracht8/opdracht8.js
@@ -1,14 +1,16 @@
 
-document.getElementById("button").addEventListener("click", function () {
+document.getElementById("btn").addEventListener("click", name());
+
+function name() {
 
     var person = prompt("Please enter a name", "Enter a name");
     var person2 = prompt("Please an other name", "Enter a name");
 
-    if (person != null) {
-        document.getElementById("demo").innerHTML = person + ", " + person2;
-    }
+    // if (person != null) {
+    document.getElementById("demo").innerHTML = person + ", " + person2;
+    // }
 
-});
+}
 
 
 
